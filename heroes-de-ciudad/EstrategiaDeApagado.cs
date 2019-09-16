17157373dce576dc095c19d29dbd17661b0a9796
Nicolas Caro <45347839+n-caro@users.noreply.github.com>
2019-09-16 04:05:08 -0300

    Fix nombre archivos de clases

--- heroes-de-ciudad/EstrategiaDeApagado.cs
+++ heroes-de-ciudad/EstrategiaDeApagado.cs
@@ -6,43 +6,9 @@ using System.Threading.Tasks;
 
 namespace heroes_de_ciudad
 {
-    class ApagadoSecuencial : EstrategiaDeApagado
+    interface EstrategiaDeApagado
     {
-        public void RecorrerLugar(ILugar lugar, Calle calle)
-        {
-            // Obtengo las dimensiones de lugar
-            int[,] matrizLugar = lugar.getSectores();
-            int caudalAgua = calle.CaudalAgua;
-
-            int lugarFilas = matrizLugar.GetLength(0);
-            int lugarColumnas = matrizLugar.GetLength(1);
-
-            for (int fila = 0; fila < lugarFilas; fila++)
-            {
-                for (int columna = 0; columna < lugarColumnas; columna++)
-                {
-                    ApagarSector(fila, columna, matrizLugar, caudalAgua);
-                }
-            }
-        }
-
-        private void ApagarSector(int fila, int columna, int[,] matrizLugar, int caudalAgua)
-        {
-            int porcentajeIncendio = matrizLugar[fila, columna];
-            StringBuilder estado_Incendio = new StringBuilder();
-            if (porcentajeIncendio > caudalAgua)
-            {
-                int porcentajeIncendioRestante = porcentajeIncendio - caudalAgua;
-                estado_Incendio.Append("-> ");
-                estado_Incendio.Append(porcentajeIncendioRestante);
-                while (porcentajeIncendioRestante > caudalAgua)
-                {
-                    porcentajeIncendioRestante -= caudalAgua;
-                    estado_Incendio.Append("-> ");
-                    estado_Incendio.Append(porcentajeIncendioRestante);
-                }
-            }
-            Console.WriteLine("     ({0},{1}) -> {2} {3}-> 0", fila, columna, porcentajeIncendio, estado_Incendio);
-        }
+        void RecorrerLugar(ILugar lugar, Calle calle);
     }
+
 }
